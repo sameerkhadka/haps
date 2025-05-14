31f4488bca307ccb351b2470e672aba45904694a
sameerkhadka <sameerkhadka3@gmail.com>
2025-05-14 21:42:25 +0545

    if else

--- resources/views/pages/projectDetail.blade.php
+++ resources/views/pages/projectDetail.blade.php
@@ -73,7 +73,7 @@
             <div class="resources_card uk-margin-medium-top">
                 <div class="uk-grid uk-child-width-1-3@m uk-child-width-1-2@s">
                     @foreach($resources as $res)
-                    @if($res->file != null || $res->file != '[]')
+                    @if($res->file != null && $res->file != '[]')
                     <div class="uk-transition-toggle">  
                         <div class="project__card">
                             <div class="projectCard__image">
