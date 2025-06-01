74c385b36a9fec5cf72a8b1a8a62b58c21244710
neu-astha <neuastha12@gmail.com>
2025-06-01 23:39:37 +0545

    change request form

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,26 +1,37 @@
     <header uk-sticky="show-on-up: true; animation: uk-animation-slide-top" class="header uk-sticky" >
-        <nav class="uk-navbar-container uk-navbar-transparent uk-visible@m">
+        <nav uk-dropnav delay-hide="400ms" class="uk-navbar-container uk-navbar-transparent uk-visible@m">
             <div class="uk-container uk-container-xlarge">
                 <div uk-navbar="" class="uk-navbar">
 
                     <div class="uk-navbar-left">
-
-                        <ul class="uk-navbar-nav">
+                        
+                        <ul class="uk-subnav">
                             <li>
                                 <a href="/" class="uk-navbar-item uk-logo">
                                     <img class="full" src="{{Voyager::image(setting('site.logo'))}}" alt="{{setting('site.title')}}">
                                 </a>
                             </li>
                             <li @if(!Request::segment(1)) class="uk-active @endif"><a href="/">Home</a></li>
-                            <li @if(Request::segment(1) == 'about') class="uk-active" @endif><a href="/about">About Us</a></li>
+                            <li @if(Request::segment(1) == 'about') @endif>
+                                <a href>About <span uk-drop-parent-icon></span></a>
+                                <div class="uk-dropdown">
+                                    <ul class="uk-nav uk-dropdown-nav">
+                                        <li @if(Request::segment(1) == 'about') class="uk-active" @endif><a href="/about" >About Us</a></li>
+                                        <li @if(Request::segment(1) == 'news') class="uk-active" @endif><a href="/news" >News </a></li>
+                                        <li @if(Request::segment(1) == 'gallery') class="uk-active" @endif><a href="/gallery">Photo Gallery</a></li>
+                                        <li @if(Request::segment(1) == 'gallery') class="uk-active" @endif><a href="/gallery">Video Gallery</a></li>
+                                        <li @if(Request::segment(1) == 'volunteer') class="uk-active" @endif><a href="/volunteer#faqs">FAQ</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                             @foreach($global_project as $pro)
-                            <li @if(Request::segment(2) == $pro->slug) class="uk-active" @endif><a href="/projects/{{$pro->slug}}">{{$pro->menu_title}}</a></li>
+                            <li @if(Request::segment(2) == $pro->slug) class="uk-active" @endif>
+                                <a href="/projects/{{$pro->slug}}">{{$pro->menu_title}}</a>
+                            </li>
                             @endforeach
                             
                             <li @if(!Request::segment(2) && Request::segment(1) == 'projects') class="uk-active" @endif><a href="/projects">Disaster Response</a></li>
-                            <li @if(Request::segment(1) == 'news') class="uk-active" @endif><a href="/news" >News </a></li>
-                            <li @if(Request::segment(1) == 'gallery') class="uk-active" @endif><a href="/gallery">Gallery</a></li>
-                            <li @if(Request::segment(1) == 'volunteer') class="uk-active" @endif><a href="/volunteer#faqs">FAQ</a></li>
+                            
                         </ul>
 
                     </div>
@@ -34,7 +45,7 @@
                     </div>
 
                     <div class="uk-navbar-right">
-                        <a href="{{setting('contact-us.facebook')}}" class="uk-navbar-item" target="_blank">
+                        <!-- <a href="{{setting('contact-us.facebook')}}" class="uk-navbar-item" target="_blank">
                             <span uk-icon="icon: facebook" class="uk-icon"></span>
                         </a>
                         <a href="{{setting('contact-us.youtube')}}" class="uk-navbar-item" target="_blank">
@@ -42,7 +53,7 @@
                         </a>
                         <a href="{{setting('contact-us.instagram')}}" class="uk-navbar-item" target="_blank">
                             <span uk-icon="icon: instagram" class="uk-icon"></span>
-                        </a>
+                        </a> -->
                         <div class="cta">
                             <a href="/donate" class="btn">Donate</a>
                         </div>
@@ -83,16 +94,63 @@
               <div class="uk-offcanvas-bar">
                 <button class="uk-offcanvas-close uk-icon uk-close" type="button" uk-close=""></button>
                 <div class="uk-panel uk-margin-medium-top">
-                    <ul class="uk-nav">
-                        <li class="uk-active"><a href="/">Home</a></li>
-                        <li><a href="/about">About Us</a></li>
-                        <li><a href="/projects/nepal-life-savers-programme">CPR</a></li>
-                        <li><a href="/projects/poison-center-collaborative">PCC</a></li>
-                        <li><a href="/projects/rural-trauma-care">Rural trauma</a></li>
-                        <li><a href="/projects">Disaster Response</a></li>
-                        <li><a href="/news" >News Coverage</a></li>
-                        <li><a href="/gallery">Gallery</a></li>
-                        <li><a href="/volunteer#faqs">FAQ</a></li>
+                    <ul uk-dropnav delay-hide="400ms" class="uk-nav">
+                        <li><a href="/">Home</a></li>
+                        <li>
+                            <a href="">About <span uk-drop-parent-icon></span></a>
+                            <div class="uk-dropdown uk-drop">
+                                <ul class="uk-nav uk-dropdown-nav">
+                                    <li><a href="/about" >About Us</a></li>
+                                    <li><a href="/news" >News Coverage</a></li>
+                                    <li><a href="/gallery" >Photo Gallery</a></li>
+                                    <li><a href="/gallery" >Video Gallery</a></li>
+                                    <li><a href="/volunteer#faqs">FAQ</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a>Nepal Life Savers Program <span uk-drop-parent-icon></span></a>
+                            <div uk-dropdown="mode: click" class="uk-navbar-dropdown uk-drop">
+                                <ul class="uk-nav uk-dropdown-nav">
+                                    <li><a href="/" >Cardiac Arrest Registry</a></li>
+                                    <li><a href="/">International Life Savers Program</a></li>
+                                    <li><a href="/">Workshop and findings</a></li>
+                                    <li><a href="/">Launch of Nepali CPR Song</a></li>
+                                </ul>                   
+                            </div>
+                        </li>                  
+                        <li>
+                            <a href="/projects/poison-center-collaborative">PCC <span uk-drop-parent-icon></span></a>
+                            <div class="uk-navbar-dropdown uk-drop">
+                                <ul class="uk-nav uk-dropdown-nav">
+                                    <li><a href="/" >Poison Information Center</a></li>
+                                    <li><a href="/">National & Global Collaboration</a></li>
+                                    <li><a href="/">Education & Awareness</a></li>
+                                    <li><a href="/">Research</a></li>
+                                    <li><a href="/">International Presentations</a></li>
+                                </ul>                   
+                            </div>
+                        </li>
+                        <li>
+                            <a href="/projects/rural-trauma-care">Rural trauma <span uk-drop-parent-icon></span></a>
+                            <div class="uk-navbar-dropdown uk-drop">
+                                <ul class="uk-nav uk-navbar-dropdown-nav">
+                                    <li><a href="/" >Community Health Workers</a></li>
+                                    <li><a href="/" >Research</a></li>
+                                    <li><a href="/" >Ultrasound</a></li>
+                                    <li><a href="/" >Videos with Global Health Media</a></li>
+                                    <li><a href="/" >International faculty/ Visitor</a></li>
+                                </ul>                   
+                            </div>
+                        </li>
+                        <li>
+                            <a>Disaster Response <span uk-drop-parent-icon></span></a>
+                            <div class="uk-navbar-dropdown uk-drop">
+                                <ul class="uk-nav uk-navbar-dropdown-nav">
+                                    <li><a href="/">2015 Earthquake</a></li>
+                                    <li><a href="/">Other Projects
+                            </div>
+                        </li>
                     </ul>
                 </div>
               </div>
