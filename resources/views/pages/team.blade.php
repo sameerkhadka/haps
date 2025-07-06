a74483e6cb96f76fa21e0032b3f3b30c44c5a5f0
neu-astha <neuastha12@gmail.com>
2025-07-06 11:20:19 +0545

    team page done

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -10,29 +10,132 @@
 @section('body')
 
 
-    <!-- <section class="team p-1">
+    <section class="team p-1">
         <div class="uk-container">
             <div class="uk-margin-auto uk-margin-auto-vertical uk-width-3-5@s">
-                <h2>{{$item->team_title}}</h2>
-                <p class="uk-margin-medium-bottom uk-text-center">{{$item->team_text}}
-                </p>
+                <h2>Our Team</h2>
+                <p class="uk-margin-medium-bottom uk-text-center">This is our Team</p>
             </div>
             
-            <div class="uk-grid uk-child-width-1-4@m uk-child-width-1-2@s">
-                @foreach($team as $team)
+            <div class="uk-grid uk-child-width-1-4@m uk-child-width-1-2@s uk-margin-medium-top uk-margin-xlarge-bottom">
+                <h3>Executive Members</h3>
                 <div class="team__wrap uk-margin-top">
                     <div class="team__photo">
-                        <img src="{{VOyager::image($team->image)}}" alt="">
+                        <img src="{{asset('images/apf.png')}}" alt="">
                     </div>
                     <div class="about__team">
-                        <div class="desg">{{$team->designation}}</div>
-                        <h4>{{$team->name}}</h5>
-                        <span>{{$team->education}}</span>
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
                     </div>
                 </div>
-                @endforeach
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="uk-grid uk-child-width-1-4@m uk-child-width-1-2@s">
+                <h3>Board Members</h3>
+                <div class="team__wrap uk-margin-top">
+                    <div class="team__photo">
+                        <img src="{{asset('images/apf.png')}}" alt="">
+                    </div>
+                    <div class="about__team">
+                        <div class="desg">Designation</div>
+                        <h4>Team Name</h5>
+                        <span>Education</span>
+                    </div>
+                </div>
+                
             </div>
         </div>
-    </section> -->
+    </section>
 
 @endsection
